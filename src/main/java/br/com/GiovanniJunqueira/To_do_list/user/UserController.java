354cf5949b038f62d7ef9129f1894d4b60461e12
Giovanni Junqueira <giovanni.junqueira@hotmail.com>
2024-03-08 14:44:09 -0300

    Initial commit

---
+++ src/main/java/br/com/GiovanniJunqueira/To_do_list/user/UserController.java
@@ -0,0 +1,32 @@
+package br.com.GiovanniJunqueira.To_do_list.user;
+
+import at.favre.lib.crypto.bcrypt.BCrypt;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+    @Autowired
+    private UserRepository userRepository;
+
+
+    @PostMapping("/create")
+    public ResponseEntity create(@RequestBody UserModel usermodel){
+        var user = this.userRepository.findByUsername(usermodel.getUsername());
+        // tratar exceções dps
+        if (user != null){
+            return ResponseEntity.status((HttpStatus.BAD_REQUEST)).body("Usuário já existe");
+        }
+        var passwordcripto = BCrypt.withDefaults().hashToString(12,usermodel.getPassword().toCharArray());
+        usermodel.setPassword(passwordcripto);
+        var user01 = this.userRepository.save(usermodel);
+        return ResponseEntity.status(HttpStatus.CREATED).body("Usuário criado");
+    }
+}
---
+++ src/main/java/br/com/GiovanniJunqueira/To_do_list/user/UserModel.java
@@ -0,0 +1,25 @@
+package br.com.GiovanniJunqueira.To_do_list.user;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Data
+@Getter
+@Setter
+@Entity(name="tab_user")
+public class UserModel {
+    @Id
+    @GeneratedValue(generator="UUID")
+    private UUID id;
+    private String name;
+    @Column(unique = true)
+    private String username;
+    private String password;
+}
