77227b2ed1a4cf996f75d7da51cfd2ab4088ddb2
Giovanni Junqueira <giovanni.junqueira@hotmail.com>
2024-03-10 22:54:45 -0300

    update method

--- src/main/java/br/com/GiovanniJunqueira/To_do_list/task/TaskRepository.java
+++ src/main/java/br/com/GiovanniJunqueira/To_do_list/task/TaskRepository.java
@@ -1,9 +1,14 @@
 package br.com.GiovanniJunqueira.To_do_list.task;
 
+import br.com.GiovanniJunqueira.To_do_list.user.UserModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
 import java.util.UUID;
 
-public interface TaskRepository extends JpaRepository<TaskModel, UUID> {
 
+public interface TaskRepository extends JpaRepository<TaskModel, UUID> {
+    TaskModel findByIdUser(UUID idUser);
+    Optional<TaskModel> findById(UUID id);
+    //porque usar o optional?
 }
--- src/main/java/br/com/GiovanniJunqueira/To_do_list/user/UserRepository.java
+++ src/main/java/br/com/GiovanniJunqueira/To_do_list/user/UserRepository.java
@@ -2,9 +2,10 @@ package br.com.GiovanniJunqueira.To_do_list.user;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface UserRepository extends JpaRepository <UserModel, UUID>{
    UserModel findByUsername (String username);
-
+   Optional<UserModel> findById (UUID id);
 }
